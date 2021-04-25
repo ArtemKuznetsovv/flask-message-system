2b5d2f7881225e44af2d61392695e3216c8cf3a4
Artem Kuznetzov <59560231+ArtemKuznetsovv@users.noreply.github.com>
2021-04-25 15:24:40 +0300

    final

--- message_system/__init__.py
+++ message_system/__init__.py
@@ -12,13 +12,7 @@ def create_app():
     app.config["SESSION_TYPE"] = "filesystem"
     app.config['SECRET_KEY'] = "secret"
 
-    USERNAME = "postgres"
-    PASSWORD = "Elb7302it!"
-    HOST_NAME = "localhost"
-    DB_NAME = "herolotask"
-
-    #os.environ.get("DATABASE_URL")
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{USERNAME}:{PASSWORD}@{HOST_NAME}:5432/{DB_NAME}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
     db.init_app(app)
 
     from message_system.models import User, Message
